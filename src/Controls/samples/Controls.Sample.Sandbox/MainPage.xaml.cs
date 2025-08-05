c2a29e54502288e8fdf081374d559d5703cd34a6
Jeremy Powell <jeremy@visionaid.com>
2025-08-05 21:23:03 +1200

    Sandbox repro

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -1,9 +1,34 @@
-﻿namespace Maui.Controls.Sample;
+﻿using System.Diagnostics;
+
+namespace Maui.Controls.Sample;
 
 public partial class MainPage : ContentPage
 {
+	bool alerted = false;
+
 	public MainPage()
 	{
 		InitializeComponent();
+	}
+
+	void OnPinchUpdated1(object sender, PinchGestureUpdatedEventArgs e)
+	{
+		if (e.Status != GestureStatus.Running)
+			Debug.WriteLine($"Pinch Updated: Status: {e.Status}");
+	}
+
+	async void OnPinchUpdated2(object sender, PinchGestureUpdatedEventArgs e)
+	{
+		if (e.Status != GestureStatus.Running)
+			Debug.WriteLine($"Pinch Updated: Status: {e.Status}");
+
+		if (e.Status == GestureStatus.Started)
+		{
+			if (!alerted)
+			{
+				alerted = true;
+				await DisplayAlert("Pinch Updated", "Message", "OK");
+			}
+		}
 	}
 }

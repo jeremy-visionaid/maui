0d7866e65a405b694157a098166426aba3220a35
Jeremy Powell <jeremy@visionaid.com>
2026-01-13 14:54:22 +1300

    CollectionView selection sandbox repro

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -1,9 +1,22 @@
 ﻿namespace Maui.Controls.Sample;
 
+public record struct TestRecord(string Name);
+
 public partial class MainPage : ContentPage
 {
+	public TestRecord[] TestItems { get; set; } =
+		[
+			new TestRecord("Item 1"),
+			new TestRecord("Item 2"),
+			new TestRecord("Item 3")
+		];
+
+	public TestRecord CurrentItem => TestItems.First();
+
 	public MainPage()
 	{
 		InitializeComponent();
+
+		BindingContext = this;
 	}
 }
